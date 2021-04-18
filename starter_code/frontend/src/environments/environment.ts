1b3d535f8ed0e108f12bacead3df168f2701c700
wythe0513 <wythenshawe0513@gmail.com>
2021-04-19 00:49:40 +0900

    update

--- starter_code/frontend/src/environments/environment.ts
+++ starter_code/frontend/src/environments/environment.ts
@@ -8,7 +8,7 @@ export const environment = {
   auth0: {
       url: 'tomascap.jp.auth0.com', // the auth0 domain prefix
       audience: 'http://localhost:5000', // the audience set for the auth0 app
-      clientId: 'ncfMphvrZRGs4Yw6zPvCW11J5GMa6ycW', // the client id generated for the auth0 app
+      clientId: 'cMffX5nbwcMWSuKw0zde7MAYrX3WEBff', // the client id generated for the auth0 app
       callbackURL: 'http://localhost:8100, http://localhost:8100/tabs/user-page', // the base url of the running ionic application. 
   }
 };
